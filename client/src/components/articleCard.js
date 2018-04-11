9c5b580911cc830bef16bc5b452aaf1932a7638d
Richard Paez <Richardp809@gmail.com>
2018-04-11 15:36:33 -0400

    added styling

--- client/src/components/articleCard.js
+++ client/src/components/articleCard.js
@@ -2,13 +2,30 @@ import React from 'react';
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
+const styles = {
+    card: {
+        position: 'relative',
+        height: '100px'
+    },
+    cardHeader: {
+        textAlign: 'left',
+        paddingRight: '15px'
+    },
+    cardActions: {
+        position: 'absolute',
+        right: '15px',
+        bottom: '0'
+    }
+}
+
 const ArticleCard = (props) => (
-    <Card>
+    <Card style={styles.card}>
         <CardHeader
         title={props.title}
-        subtitle={props.subtitle}
+        subtitle={props.link}
+        style={styles.cardHeader}
         />
-        <CardActions>
+        <CardActions style={styles.cardActions}>
             <FlatButton label="Context" />
             <FlatButton label="Comments" />
         </CardActions>
