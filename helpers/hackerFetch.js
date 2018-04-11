21917b0a75efdd24fdc84f56ad7e740da639122a
Richard Paez <Richardp809@gmail.com>
2018-04-10 21:03:09 -0400

    scrapeArticles now added to take the raw html and parse it into objects

--- helpers/hackerFetch.js
+++ helpers/hackerFetch.js
@@ -1,7 +1,9 @@
-const https = require('https');
+const https     = require('https');
+const cheerio   = require('cheerio');
 
 const url = 'https://news.ycombinator.com/';
 
+// fetch the html
 const getHtml = new Promise( ( resolve, reject ) => {
 
     https.get(url, response => {
@@ -19,6 +21,36 @@ const getHtml = new Promise( ( resolve, reject ) => {
 
 });
 
+const scrapeArticles = new Promise( ( resolve, reject ) => {
+    // parse the html with cheerio
+    getHtml.then( html => {
+        const $ = cheerio.load( html );
+
+        const results = $(".athing");
+
+        const resultsArray = [];
+
+        for (let property in results) {
+            if (results.hasOwnProperty(property) && parseInt(property) ) {
+                resultsArray.push( results[property] );
+            }
+        }
+
+        const cleanResults = resultsArray.map( result => {
+            return {
+                id: result.attribs.id,
+                title: result.children[4].children[0].children[0].data,
+                link: result.children[4].children[0].attribs.href,
+                context: `"${url}item?id=${result.attribs.id}"`
+            }
+        } );
+
+        resolve(cleanResults);
+
+    }).catch( error => { reject(error) } );
+
+});
+
 module.exports = {
-    getHtml : getHtml
-}
+    fetchArticles : scrapeArticles
+};
