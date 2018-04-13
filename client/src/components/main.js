78fc303572ccb928a09f21e994561a14a4eb01aa
Richard Paez <Richardp809@gmail.com>
2018-04-13 17:17:49 -0400

    added a route for logging in

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './Home/Home';
+import Login from './Login/Login';
 
 const Main = () => (
     <main>
         <Switch>
             <Route exact path="/" component={Home}/>
+            <Route exact path="/login" component={Login}/>
         </Switch>
     </main>
 );
