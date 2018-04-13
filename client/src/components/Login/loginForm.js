bc1c4580fabc6e4ae2b7c86a2c411f1f992cf53a
Richard Paez <Richardp809@gmail.com>
2018-04-13 17:17:22 -0400

    added a functional stateless loginForm

---
+++ client/src/components/Login/loginForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const loginBtnStyles = {
+    marginTop: "50px"
+}
+
+const signUpBtnStyles = {
+    marginTop: "50px",
+    marginLeft: "5px"
+}
+
+const LoginForm = (props) => {
+    return (
+        <form>
+            <TextField
+                floatingLabelText="Username"
+                onChange={props.onUsernameChange}
+                errorText={props.usernameError} />
+            <br/>
+            <TextField
+                floatingLabelText="Password"
+                type="password"
+                onChange={props.onPasswordChange} 
+                errorText={props.passwordError} />
+            <br />
+            <RaisedButton 
+                label="Login" 
+                type="submit"
+                primary={true} 
+                style={loginBtnStyles}
+                onClick={props.onSubmit}
+                disabled={props.submitBtnDisabled} /> or 
+            <RaisedButton 
+                label="Sign Up"
+                style={signUpBtnStyles}/>
+        </form>
+    );
+}
+
+export default LoginForm;
