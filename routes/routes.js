3edfb0a82c65640d620a1410516e90533b2e948f
Richard Paez <Richardp809@gmail.com>
2018-04-13 16:14:18 -0400

    added routes for sign up and sing in

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,13 @@
 const articles = require('../controllers/article-controller');
+const users = require('../controllers/user-controller');
 
 module.exports = ( app ) => {
     app.route('/api/articles')
         .get( articles.list_all_articles );
-}
+
+    app.route('/api/user/signup')
+        .post( users.create_a_user );
+
+    app.route('/api/user/signin')
+        .post( users.sign_in );
+};
