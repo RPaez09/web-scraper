ddca384b4603ab586e36c14960d25225b04f96ba
Richard Paez <Richardp809@gmail.com>
2018-04-12 22:49:35 -0400

    added passport

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser    = require('body-parser');
 const morgan        = require('morgan');
 const mongoose      = require('mongoose');
 const routes        = require('./routes/routes');
+const passport      = require('passport');
 
 const config        = require('./config/config');
 
@@ -26,6 +27,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
   });
+app.use(passport.initialize());
 
 routes(app);
 
