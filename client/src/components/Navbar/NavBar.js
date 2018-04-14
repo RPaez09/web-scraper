54a4e3b43d22f51525447ba58d51c9c95b4b0537
Richard Paez <Richardp809@gmail.com>
2018-04-14 00:53:35 -0400

    removed unneeded constructor

--- client/src/components/Navbar/NavBar.js
+++ client/src/components/Navbar/NavBar.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 
 export default class Navbar extends Component {
-
+/*
     constructor(props){
         super(props);
     }
-
+*/
     render(){
         return (
             <AppBar
