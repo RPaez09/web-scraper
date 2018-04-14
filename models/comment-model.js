17dcc33d5b9640d480dd4eba34403e57a9af60b4
Richard Paez <Richardp809@gmail.com>
2018-04-14 01:25:16 -0400

    added a model for comments

---
+++ models/comment-model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    articleId: {
+        type: String,
+        required: 'A target Article ID is required.'
+    },
+    username: {
+        type: String,
+        required: 'A username is required.'
+    },
+    text: {
+        type: String,
+        required: 'A comment is required.'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
