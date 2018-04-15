03f7437c53ba587a937193c5e75d517ed845bc4f
Richard Paez <Richardp809@gmail.com>
2018-04-15 00:27:09 -0400

    updated with better styling and navigation

--- client/src/components/Navbar/NavBar.js
+++ client/src/components/Navbar/NavBar.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
 
-export default class Navbar extends Component {
-/*
-    constructor(props){
-        super(props);
-    }
-*/
-    render(){
-        return (
-            <AppBar
-                title="Hacker News"
-                showMenuIconButton={false} />
-        )
-    }
+const TitleStyles = {
+    cursor: "pointer",
+    marginLeft: "0",
+    flex: "none"
+};
+
+const LoginStyles = {
+    color: "#FFFFFF",
+    marginTop: "5px"
 }
+
+const Navbar = ( { history } ) => {
+    return (
+        <AppBar
+            title={<span> Hacker News</span>}
+            showMenuIconButton={false}
+            titleStyle={TitleStyles}
+            onTitleClick={() => history.push('/')}
+            iconElementRight={<Link to="/login"><FlatButton label="Login" style={LoginStyles} /></Link>} />
+    )
+}
+
+export default withRouter(Navbar);
