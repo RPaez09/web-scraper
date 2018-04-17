f14409bb15c77681628092532f3989d15ee1ee42
Richard Paez <Richardp809@gmail.com>
2018-04-17 15:58:20 -0400

    helper function for extracting a jwt token from q request

---
+++ helpers/getToken.js
@@ -0,0 +1,13 @@
+module.exports = ( headers ) => {
+    if( headers && headers.authorization ){
+        var parted = headers.authorization.split(' ');
+        if( parted.length === 2 ) {
+            return parted[1];
+        }
+        else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+}
