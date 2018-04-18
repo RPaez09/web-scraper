409a4bca9f3db3feb3c152e179655586ce2c0b12
Richard Paez <Richardp809@gmail.com>
2018-04-17 20:48:43 -0400

    created controller for dealing with favorites

---
+++ controllers/favorite-controller.js
@@ -0,0 +1,76 @@
+const mongoose  = require('mongoose');
+const favorite  = require('../models/favorite-model');
+const jwt       = require('jsonwebtoken');
+const getToken  = require('../helpers/getToken');
+
+exports.get_user_favorites = ( req, res ) => {
+    const token = getToken( req.headers );
+
+    if( token ){
+
+        user = jwt.verify( token, process.env.JWT_SECRET );
+
+        if( user._id === req.params.userID ){
+            favorite.find({ userID: user._id })
+                .sort({ date: -1 })
+                .then( favorites => res.send( favorites ) )
+                .catch( error => console.log( `Error: ${error}` ) );
+        } else {
+            res.status(403).send( { success: false, msg: 'Unauthorized' } );
+        }
+
+    } else {
+        res.status(403).send( { success: false, msg: 'Unauthorized' } );
+    }
+};
+
+exports.add_a_favorite = ( req, res ) => {
+    const token = getToken( req.headers );
+
+    if( token ){
+
+        user = jwt.verify( token, process.env.JWT_SECRET );
+
+        if( user._id === req.body.userID ){
+            let newFavorite = {
+                userID : user._id,
+                articleID : req.body.articleID
+            }
+
+            favorite.create( newFavorite )
+                .then( freshFavorite => res.send( freshFavorite ) )
+                .catch( error => console.log( `Error: ${error}` ) );
+        } else {
+            res.status(403).send( { success: false, msg: 'Unauthorized' } );
+        }
+    } else {
+        res.status(403).send( { success: false, msg: 'Unauthorized' } );
+    }
+
+};
+
+exports.remove_a_favorite = ( req, res ) => {
+    const token = getToken( req.headers );
+
+    if( token ){ // do they have a token at all
+
+        user = jwt.verify( token, process.env.JWT_SECRET );
+
+        favorite.findById( req.body._id )
+            .then( result => {
+                if( result.userID === user._id ){ // if this favorite belongs to you
+
+                    favorite.findByIdAndRemove( { _id: req.body._id } )
+                        .then( deletedFavorite => res.send( deletedFavorite ) )
+                        .catch( error => console.log( `Error: ${error}` ) );
+                
+                } else {
+                    res.status(403).send( { success: false, msg: 'Unauthorized' } ); // how dare you...
+                }
+            })
+            .catch( error => console.log( `Error: ${error}` ) );
+    } else {
+        res.status(403).send( { success: false, msg: 'Unauthorized' } );
+    }
+
+};
