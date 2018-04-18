9f3ef4eb1874b96780d1d807d459de5f87e97841
Richard Paez <Richardp809@gmail.com>
2018-04-17 21:39:21 -0400

    A page to display your saved articles

---
+++ client/src/components/SavedArticles/SavedArticles.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+
+import ArticleList from '../Home/articleList';
+import CircularProgress from 'material-ui/CircularProgress';
+
+import API from '../../api';
+
+export default class SavedArticles extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            articles: [],
+            isLoading: true
+        }
+    };
+
+    componentDidMount(){
+        if( this.props.user.isLoggedIn ){
+            let options = { headers: { authorization: this.props.user.token } };
+            API.get(`/api/favorites/${this.props.user.id}`, options)
+                .then( response => {
+                    if(response.status === 200){
+                        this.setState({
+                            articles: response.data,
+                            isLoading: false
+                        });
+                    }
+                })
+                .catch( error => console.log(error) );
+        }
+    };
+
+    render(){
+        return (
+        <React.Fragment>
+            { !this.props.user.isLoggedIn && <Redirect to="/" push /> }
+            { (this.state.isLoading) ? <CircularProgress size={80} thickness={5} /> : <ArticleList articles={this.state.articles} user={this.props.user}/>}
+        </React.Fragment>)
+    }
+}
